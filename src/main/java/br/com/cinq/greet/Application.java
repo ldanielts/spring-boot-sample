cd01a6b619c562457928d222484d909e01113b17
Daniel Tavares <ldanielts@gmail.com>
2016-07-08 02:07:39 -0300

    Update Application.java

--- src/main/java/br/com/cinq/greet/Application.java
+++ src/main/java/br/com/cinq/greet/Application.java
@@ -3,11 +3,13 @@ package br.com.cinq.greet;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Greeting Service.
  */
 @SpringBootApplication
+@ComponentScan(basePackages="br.com.cinq.greet")
 public class Application extends SpringBootServletInitializer {
 
 	@Override
